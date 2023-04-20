06bcbc5bc8c9cc9b075219f8e0df6d70394627fc
swarup-1 <swarupkadoli9@gmail.com>
2023-04-21 00:40:22 +0530

    Loading Indicator Changed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false)
-    }, 2700);
+    }, 2000);
     return () => clearTimeout(timer);
   }, []);
   return loading ? (
