cf11aa99a03045e5a17fa32f9d46a315c0a38d30
swarup-1 <swarupkadoli9@gmail.com>
2023-05-06 21:15:57 +0530

    SEO

--- src/App.js
+++ src/App.js
@@ -29,15 +29,15 @@ function App() {
   (
     <div className="App">
       <Helmet>
-        <title>Swarup Kadoli - MERN Stack Developer</title>
+        <title>Swarup Kadoli Portfolio</title>
         <meta name="description" content="Welcome to Swarup's portfolio website. Explore a diverse collection of web development projects, showcasing my expertise in the MERN stack. From building responsive front-end designs to developing complex back-end functionality, I combine technical knowledge with creative vision to produce exceptional results. Check out my portfolio to see examples of my work and learn more about my experience and skills as a developer. Contact me today to bring your next project to life with cutting-edge web technology." />
-        <meta name="author" content="Swarup Kadoli" />
-        <meta name="keywords" content="Swarup Kadoli, swarup, kadoli, masai, Masai School, Web developer, portfolio, MERN Stack Developer, web development projects, front-end designs, back-end functionality, technical knowledge, creative vision, exceptional results, portfolio, experience, skills, cutting-edge web technology" />
+        <meta name="author" content="Swarup Kadoli - MERN Stack Developer" />
+        <meta name="keywords" content="Swarup Kadoli Portfolio, Swarup Kadoli, swarup, kadoli, masai, Masai School, Web developer, portfolio, MERN Stack Developer, web development projects, front-end designs, back-end functionality, technical knowledge, creative vision, exceptional results, portfolio, experience, skills, cutting-edge web technology" />
         <meta property="og:title" content="Swarup Kadoli - MERN Stack Developer" />
         <meta property="og:type" content="website" />
         <meta property="og:description" content="Welcome to Swarup's portfolio website. Explore a diverse collection of web development projects, showcasing my expertise in the MERN stack. From building responsive front-end designs to developing complex back-end functionality, I combine technical knowledge with creative vision to produce exceptional results. Check out my portfolio to see examples of my work and learn more about my experience and skills as a developer. Contact me today to bring your next project to life with cutting-edge web technology." />
         <meta property="og:url" content="https://swarup-1.github.io/" />
-        <meta property="og:site_name" content="Swarup Kadoli | Portfolio" />
+        <meta property="og:site_name" content="Swarup Kadoli Portfolio" />
         <meta property="og:locale" content="en_US" />
         <meta property="profile:first_name" content="Swarup" />
         <meta property="profile:last_name" content="Kadoli" />
